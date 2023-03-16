b1e35d0c121e60ca62f1da380e20f76b75ad04d7
Moura <guilherme4gamerxd@gmail.com>
2023-03-16 11:47:35 +0000

    Eleventh Commit

---
+++ app/src/main/java/com/example/yourpills/Rotinas_habaActivity.java
@@ -0,0 +1,14 @@
+package com.example.yourpills;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rotinas_habaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rotinas_haba);
+    }
+}
