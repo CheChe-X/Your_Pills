f61a041f6869d0ae89cd8e736d04db3362162ae3
Moura <guilherme4gamerxd@gmail.com>
2023-04-10 18:05:00 +0100

    Código acabado e explicado

--- app/src/main/java/com/example/yourpills/CalendarAdapter.java
+++ app/src/main/java/com/example/yourpills/CalendarAdapter.java
@@ -24,21 +24,38 @@ import java.util.ArrayList;
 
 class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>
 {
+    //este código vai definir uma variável final chamada "days"
+    //este código é usado para armazenar uma lista de datas que serão exibidas no calendário
     private final ArrayList<LocalDate> days;
+
+    //este código vai definir uma variável final chamada "onItemListener"
+    //este código é usado para capturar eventos de clique do usuário em um item do calendário
     private final OnItemListener onItemListener;
 
+    //está classe de adaptador é tipicamente usada em conjunto com a classe RecyclerView para
+    // exibir uma lista de datas em um layout de calendário personalizado.
+    // O construtor é chamado quando o adaptador é instanciado
+    // e define as variáveis days e onItemListener com as entradas fornecidas.
     public CalendarAdapter(ArrayList<LocalDate> days, OnItemListener onItemListener)
     {
         this.days = days;
         this.onItemListener = onItemListener;
     }
 
+    //este código é responsável por criar a visualização de cada célula do calendário
     @NonNull
     @Override
     public CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
+        //O primeiro passo é criar um objeto LayoutInflater usando o contexto do ViewGroup pai passado como parâmetro
+        //Isso permite que o código infle o layout da célula do calendário,
+        //que é definido em um arquivo XML (calendar_cell.xml)
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        //O método inflate() é usado para criar uma instância da view da célula do calendário
+        // usando o layout definido em calendar_cell.xml
         View view = inflater.inflate(R.layout.calendar_cell, parent, false);
+        //o código obtém os parâmetros de layout da view recém-criada
+        // e ajusta sua altura com base no número de dias no mês
         ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
         if(days.size() > 15) //month view
             layoutParams.height = (int) (parent.getHeight() * 0.166666666);
@@ -48,28 +65,41 @@ class CalendarAdapter extends RecyclerView.Adapter<CalendarViewHolder>
         return new CalendarViewHolder(view, onItemListener, days);
     }
 
+    //este código é responsável por vincular os dados de um objeto CalendarViewHolder
+    //com um item específico na lista de datas exibida pelo adaptador
     @Override
     public void onBindViewHolder(@NonNull CalendarViewHolder holder, int position)
     {
+        //
         final LocalDate date = days.get(position);
+        //Se a data for nula, significa que não há um dia correspondente àquela posição
+        //então o texto do dia do mês é definido como vazio no TextView correspondente
         if(date == null)
             holder.dayOfMonth.setText("");
         else
         {
+            //Caso contrário, o dia do mês é definido como o dia do mês da data correspondente
+            //se a data correspondente for igual à data selecionada
             holder.dayOfMonth.setText(String.valueOf(date.getDayOfMonth()));
             if(date.equals(CalendarUtils.selectDate))
                 holder.parentView.setBackgroundColor(Color.LTGRAY);
         }
     }
 
+    //Este código é parte de uma classe que implementa o RecyclerView.Adapter no Android Studio
+    //e o método getItemCount() é responsável por retornar o número de itens que o RecyclerView irá exibir
     @Override
     public int getItemCount()
     {
         return days.size();
     }
 
+    //este código define uma interface OnItemListener que declara um método onItemClick que recebe dois parâmetros
+    //a posição do item na lista e a data correspondente a esse item na lista
     public interface  OnItemListener
     {
+        //está interface é utilizada para permitir que a classe CalendarAdapter possa se comunicar
+        //com a atividade que a utiliza, notificando-a quando um item da lista é clicado
         void onItemClick(int position, LocalDate date);
     }
 }
--- app/src/main/java/com/example/yourpills/CalendarUtils.java
+++ app/src/main/java/com/example/yourpills/CalendarUtils.java
@@ -8,36 +8,52 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class CalendarUtils {
+    //A selectDate é uma variável que pertence a class LocalDate que representa a data e hora
     public static LocalDate selectDate;
 
+    //este código vai chamar o método formattedDate recebe um objeto LocalDate
+    // como entrada e retorna uma string formatada no formato "dd MMMM yyyy"
     public static String formattedDate(LocalDate date)
     {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
         return date.format(formatter);
     }
 
+    //este código vai chamar o método formattedTime recebe um objeto LocalTime
+    // como entrada e retorna uma string formatada no formato "hh:mm:ss a"
     public static String formattedTime(LocalTime time)
     {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss a");
         return time.format(formatter);
     }
 
+    //este código o método recebe um objeto LocalDate como entrada,
+    // que representa uma data sem hora e sem fuso horário
+    // Em seguida, ele cria um objeto DateTimeFormatter usando o padrão "MMMM yyyy"
     public static String monthYearFromDate(LocalDate date)
     {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
         return date.format(formatter);
     }
 
-    public static ArrayList<LocalDate> daysInMonthArray(LocalDate Data) {
-        ArrayList<LocalDate> daysInMonthArray = new ArrayList<>();
 
+    //Este código é um método que recebe uma data do tipo LocalDate como entrada e retorna
+    // uma lista de todas as datas dentro do mês correspondente.
+    public static ArrayList<LocalDate> daysInMonthArray(LocalDate Data) {
+        //O método começa criando um objeto ArrayList para armazenar todas as datas dentro do mês
+        // correspondente e obtém o mês e o ano
+        ArrayList<LocalDate> daysInMonthArray = new ArrayList<>();
         YearMonth yearMonth = YearMonth.from(Data);
         int daysInMonth = yearMonth.lengthOfMonth();
 
+        //neste código o método vai determinar em qual dia da semana o primeiro dia do mês começa
+        // usando withDayOfMonth(1) e getDayOfWeek().getValue()
         LocalDate firstOfMonth = CalendarUtils.selectDate.withDayOfMonth(1);
         int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
 
+        //agora o método então percorre todos os dias do mês em um loop, adicionando cada data à lista
         for(int i = 1; i <= daysInMonth + dayOfWeek; i++){
+            //Se o dia em questão não corresponder ao mês em questão, o método adiciona null à lista
             if (i <= dayOfWeek || i > daysInMonth + dayOfWeek){
                 daysInMonthArray.add(null);
             }
@@ -47,34 +63,4 @@ public class CalendarUtils {
         }
         return daysInMonthArray;
     }
-
-    public static ArrayList<LocalDate> daysInWeekArray(LocalDate selectedDate)
-    {
-        ArrayList<LocalDate> days = new ArrayList<>();
-        LocalDate current = sundayForDate(selectedDate);
-        LocalDate endDate = current.plusWeeks(1);
-
-        while (current.isBefore(endDate))
-        {
-            days.add(current);
-            current = current.plusDays(1);
-        }
-        return days;
-    }
-
-    private static LocalDate sundayForDate(LocalDate current)
-    {
-        LocalDate oneWeekAgo = current.minusWeeks(1);
-
-        while (current.isAfter(oneWeekAgo))
-        {
-            if(current.getDayOfWeek() == DayOfWeek.SUNDAY)
-                return current;
-
-            current = current.minusDays(1);
-        }
-
-        return null;
-    }
-
 }
